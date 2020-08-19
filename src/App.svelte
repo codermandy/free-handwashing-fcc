819ced27573c8e2bd652030b542c60e084724947
codermandy <kamramohit96@gmail.com>
2020-08-19 20:52:35 +0530

    got sound to work

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,36 @@
 <script>
-import Timer from "./Timer.svelte"
-import HowTo from "./HowTo.svelte"
-</script>
-
-<style>
-h1,h3 {
-    text-align:center;
-}
-</style>
-
-<h1>Handwashing App</h1>
-
-<Timer />
-<HowTo />
-
-<h3>
-    <a href="https://www.who.int/gpsc/media/how_to_handwash_lge.gif">
-    Picture Source</a>
-
-    <a href="https://freesound.org/people/metrostock99/sounds/345086/">
-    Sound Source</a>
-</h3>
+    import Timer from "./Timer.svelte";
+    import HowTo from "./HowTo.svelte";
+    let audio;
+    function timerEnds(e) {
+      audio.play();
+    }
+  </script>
+  
+  <style>
+    h1,
+    h3 {
+      text-align: center;
+    }
+  </style>
+  
+  <h1>Handwashing App</h1>
+  
+  <Timer on:end={timerEnds} />
+  
+  <HowTo />
+  
+  <h3>
+    <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+      Picture Source
+    </a>
+  
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/ ">
+      Sound Source
+    </a>
+  </h3>
+  
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <audio bind:this={audio}>
+    <source src="ohyeah.wav" />
+  </audio>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,45 +1,53 @@
 <script>
+    import { createEventDispatcher } from "svelte";
     import ProgressBar from "./ProgressBar.svelte";
-
-    const totalSec=5;
-    let secondLeft= totalSec;
+    const totalSeconds = 20;
+    let secondLeft = totalSeconds;
     let isRunning = false;
-    $:progress=((totalSec-secondLeft)/totalSec)*100;
-    function startTimer(){
-        isRunning=true;
-    const timer = setInterval(() =>
-    {
-        secondLeft-=1;
-        if(secondLeft==0) {
-            clearInterval(timer);
-            isRunning=false;
-            secondLeft=totalSec;
+    $: progress = ((totalSeconds - secondLeft) / totalSeconds) * 100;
+    const dispatch = createEventDispatcher();
+    function startTimer() {
+      isRunning = true;
+      const timer = setInterval(() => {
+        secondLeft -= 1;
+        if (secondLeft == 0) {
+          clearInterval(timer);
+          isRunning = false;
+          secondLeft = totalSeconds;
+          dispatch("end");
         }
-    },1000) ;
-
+      }, 1000);
     }
-
-</script>
-
-<style>
+  </script>
+  
+  <style>
     h2 {
-        margin:0;
+      margin: 0;
     }
     .start {
-        background-color: rgb(43,73,73);
-        width:100%;
-        margin: 10px 0;
+      background-color: rgb(154, 73, 73);
+      width: 100%;
+      margin: 10px 0;
     }
-    .start[disabled]{
-        background-color: cadetblue;
-        cursor:not-allowed;
+    .start[disabled] {
+      background-color: rgb(194, 194, 194);
+      cursor: not-allowed;
     }
-</style>
-<div bp="grid" >
-    <h2 bp="offset-5@md 4@md 12@s">Seconds Left: {secondLeft} </h2>
-</div>
-
-<ProgressBar {progress} />
-<div bp="grid" >
-<button disabled={isRunning} on:click={startTimer} bp="offset-5@md 4@md 12@s" class="start">Start</button>
-</div>
+  </style>
+  
+  <div bp="grid">
+    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondLeft}</h2>
+  </div>
+  <ProgressBar {progress} />
+  
+  <div bp="grid">
+  
+    <button
+      disabled={isRunning}
+      on:click={startTimer}
+      bp="offset-5@md 4@md 12@sm"
+      class="start">
+      Start
+    </button>
+  
+  </div>
